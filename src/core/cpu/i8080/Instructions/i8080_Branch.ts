3c28e5ea5d61752b070829fc206d27841deb6d26
Hacktix <zecmad16@htlkaindorf.at>
2022-02-16 22:29:45 +0100

    Implement i8080 Ccond

--- src/core/cpu/i8080/Instructions/i8080_Branch.ts
+++ src/core/cpu/i8080/Instructions/i8080_Branch.ts
@@ -35,6 +35,30 @@ export function i8080_CALL(this: i8080): number {
     return 17;
 }
 
+export function i8080_Ccond(this: i8080, cond: BranchCondition): number {
+    let matchCond = false;
+    switch(cond) {
+        case BranchCondition.NZ: matchCond = !this.flags.z; break;
+        case BranchCondition.Z: matchCond = this.flags.z; break;
+        case BranchCondition.NC: matchCond = !this.flags.cy; break;
+        case BranchCondition.C: matchCond = this.flags.cy; break;
+        case BranchCondition.PO: matchCond = !this.flags.p; break;
+        case BranchCondition.PE: matchCond = this.flags.p; break;
+        case BranchCondition.P: matchCond = !this.flags.s; break;
+        case BranchCondition.M: matchCond = this.flags.s; break;
+    }
+    if(matchCond) {
+        const addr = this.bus.read(this.pc) | (this.bus.read(this.pc + 1) << 8);
+        this.pc += 2;
+        this.bus.write((this.pc & 0xff00) >> 8, --this.regs.sp);
+        this.bus.write(this.pc & 0xff, --this.regs.sp);
+        this.pc = addr;
+        return 17;
+    }
+    this.pc += 2;
+    return 11;
+}
+
 export function i8080_RET(this: i8080): number {
     this.pc = this.bus.read(this.regs.sp) | (this.bus.read(this.regs.sp + 1) << 8);
     this.regs.sp += 2;
