c7f84de1dae35fed95bdf035c973aec83400ca39
Hacktix <zecmad16@htlkaindorf.at>
2022-02-16 11:36:58 +0100

    Fix LXI not incrementing PC

--- src/core/cpu/i8080/Instructions/i8080_DataTransfer.ts
+++ src/core/cpu/i8080/Instructions/i8080_DataTransfer.ts
@@ -3,5 +3,6 @@ import { RegisterPair } from "../i8080_InstructionEnums";
 
 export function i8080_LXI(this: i8080, target: RegisterPair) {
     this.regs[target] = this.bus.read(this.pc) | (this.bus.read(this.pc + 1) << 8);
+    this.pc += 2;
     return 3;
 }
