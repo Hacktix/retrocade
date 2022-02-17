7b74b9421b021eff86b3bbfa8e0c5c3713c948ff
Hacktix <zecmad16@htlkaindorf.at>
2022-02-17 10:53:54 +0100

    Fix i8080 SBI & re-order Arithmetic instructions

--- src/core/cpu/i8080/Instructions/i8080_Arithmetic.ts
+++ src/core/cpu/i8080/Instructions/i8080_Arithmetic.ts
@@ -1,67 +1,6 @@
 import i8080 from "../i8080";
 import { PARITY_LOOKUP_TABLE, Register, RegisterPair } from "../i8080_InstructionEnums";
 
-export function i8080_INR(this: i8080, target: Register): number {
-    if(target === Register.M) {
-        const incVal = (this.bus.read(this.regs.hl) + 1) & 0xff;
-        this.bus.write(incVal, this.regs.hl);
-        this.flags.z = incVal === 0;
-        this.flags.s = (incVal & 0x80) !== 0;
-        this.flags.p = PARITY_LOOKUP_TABLE[incVal];
-        this.flags.ac = (incVal & 0xf) === 0;
-        return 10;
-    }
-    else {
-        this.regs[target]++;
-        this.flags.z = this.regs[target] === 0;
-        this.flags.s = (this.regs[target] & 0x80) !== 0;
-        this.flags.p = PARITY_LOOKUP_TABLE[this.regs[target]];
-        this.flags.ac = (this.regs[target] & 0xf) === 0;
-        return 3;
-    }
-}
-
-export function i8080_INX(this: i8080, target: RegisterPair) {
-    this.regs[target]++;
-    return 5;
-}
-
-export function i8080_DCX(this: i8080, target: RegisterPair) {
-    this.regs[target]--;
-    return 5;
-}
-
-export function i8080_DCR(this: i8080, target: Register) {
-    if(target === Register.M) {
-        const src = this.bus.read(this.regs.hl);
-        const res = (src - 1) & 0xFF;
-        this.bus.write(res, this.regs.hl);
-
-        this.flags.z = res === 0;
-        this.flags.s = (res & 0x80) !== 0;
-        this.flags.p = PARITY_LOOKUP_TABLE[res];
-        this.flags.ac = (src & 0xf) === 0xf;
-        return 10;
-    }
-    else {
-        const src = this.regs[target];
-        const res = (src - 1) & 0xFF;
-        this.regs[target] = res;
-
-        this.flags.z = res === 0;
-        this.flags.s = (res & 0x80) !== 0;
-        this.flags.p = PARITY_LOOKUP_TABLE[res];
-        this.flags.ac = (src & 0xf) === 0xf;
-        return 5;
-    }
-}
-
-export function i8080_DAD(this: i8080, src: RegisterPair) {
-    this.flags.cy = (this.regs.hl + this.regs[src]) > 0xffff;
-    this.regs.hl += this.regs[src];
-    return 10;
-}
-
 export function i8080_ADD(this: i8080, src: Register): number {
     const addVal = src === Register.M ? this.bus.read(this.regs.hl) : this.regs[src];
     this.flags.cy = (this.regs.a + addVal) > 0xff;
@@ -107,7 +46,7 @@ export function i8080_SUI(this: i8080): number {
 }
 
 export function i8080_SBI(this: i8080): number {
-    const subVal = (this.bus.read(this.pc++) + 1) & 0xff;
+    const subVal = this.bus.read(this.pc++) + (this.flags.cy ? 1 : 0);
     this.flags.cy = (this.regs.a - subVal) < 0;
     this.flags.ac = (this.regs.a & 0xf) - (subVal & 0xf) < 0;
     this.regs.a -= subVal;
@@ -115,6 +54,67 @@ export function i8080_SBI(this: i8080): number {
     this.flags.s = (this.regs.a & 0x80) !== 0;
     this.flags.p = PARITY_LOOKUP_TABLE[this.regs.a];
     return 7;
+}
+
+export function i8080_INR(this: i8080, target: Register): number {
+    if(target === Register.M) {
+        const incVal = (this.bus.read(this.regs.hl) + 1) & 0xff;
+        this.bus.write(incVal, this.regs.hl);
+        this.flags.z = incVal === 0;
+        this.flags.s = (incVal & 0x80) !== 0;
+        this.flags.p = PARITY_LOOKUP_TABLE[incVal];
+        this.flags.ac = (incVal & 0xf) === 0;
+        return 10;
+    }
+    else {
+        this.regs[target]++;
+        this.flags.z = this.regs[target] === 0;
+        this.flags.s = (this.regs[target] & 0x80) !== 0;
+        this.flags.p = PARITY_LOOKUP_TABLE[this.regs[target]];
+        this.flags.ac = (this.regs[target] & 0xf) === 0;
+        return 3;
+    }
+}
+
+export function i8080_DCR(this: i8080, target: Register) {
+    if(target === Register.M) {
+        const src = this.bus.read(this.regs.hl);
+        const res = (src - 1) & 0xFF;
+        this.bus.write(res, this.regs.hl);
+
+        this.flags.z = res === 0;
+        this.flags.s = (res & 0x80) !== 0;
+        this.flags.p = PARITY_LOOKUP_TABLE[res];
+        this.flags.ac = (src & 0xf) === 0xf;
+        return 10;
+    }
+    else {
+        const src = this.regs[target];
+        const res = (src - 1) & 0xFF;
+        this.regs[target] = res;
+
+        this.flags.z = res === 0;
+        this.flags.s = (res & 0x80) !== 0;
+        this.flags.p = PARITY_LOOKUP_TABLE[res];
+        this.flags.ac = (src & 0xf) === 0xf;
+        return 5;
+    }
+}
+
+export function i8080_INX(this: i8080, target: RegisterPair) {
+    this.regs[target]++;
+    return 5;
+}
+
+export function i8080_DCX(this: i8080, target: RegisterPair) {
+    this.regs[target]--;
+    return 5;
+}
+
+export function i8080_DAD(this: i8080, src: RegisterPair) {
+    this.flags.cy = (this.regs.hl + this.regs[src]) > 0xffff;
+    this.regs.hl += this.regs[src];
+    return 10;
 }
 
 export function i8080_DAA(this: i8080): number {
