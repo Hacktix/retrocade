5626e8f203cfdf91540c3f183c56e58d6efc3896
Hacktix <zecmad16@htlkaindorf.at>
2022-02-17 14:46:31 +0100

    Make EmulatorCanvas adjust to screen size properly

--- src/components/common/EmulatorCanvas/EmulatorCanvas.tsx
+++ src/components/common/EmulatorCanvas/EmulatorCanvas.tsx
@@ -3,10 +3,19 @@ import "./EmulatorCanvas.css";
 type EmulatorCanvasProps = {
     width: number,
     height: number,
+    maxWidthCover?: number,
+    maxHeightCover?: number,
     canvasRef: React.MutableRefObject<HTMLCanvasElement | null>
 }
 
 export default function EmulatorCanvas(props: EmulatorCanvasProps) {
+    const mw = Math.round(window.innerWidth * (props.maxWidthCover || 0.9));
+    const mh = Math.round(window.innerHeight * (props.maxHeightCover || 0.6));
+    const ar = props.width / props.height;
+    const scaledSize = (mw / props.width) < (mh / props.height)
+        ? { width: mw, height: Math.round(mw / ar) }
+        : { width: Math.round(mh * ar), height: mh};
+        
     return (
         <div className="EmulatorCanvasContainer">
             <canvas
@@ -14,6 +23,7 @@ export default function EmulatorCanvas(props: EmulatorCanvasProps) {
                 ref={props.canvasRef}
                 width={props.width}
                 height={props.height}
+                style={scaledSize}
             />
         </div>
     );
