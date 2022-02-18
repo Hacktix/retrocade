1c7d934967a684c8cbd6c794b1e6f5219c70ce78
Hacktix <zecmad16@htlkaindorf.at>
2022-02-18 18:30:25 +0100

    Add neon flicker effect to RETROCADE logo

--- src/components/common/SiteHeader/SiteHeader.tsx
+++ src/components/common/SiteHeader/SiteHeader.tsx
@@ -1,3 +1,4 @@
+import { BrowserView, MobileView } from 'react-device-detect';
 import { Link } from 'react-router-dom';
 import './SiteHeader.css';
 
@@ -6,7 +7,22 @@ export default function SiteHeader() {
         <>
         <div className="HeaderBar">
             <Link to="/">
-                <h1>RETROCADE</h1>
+                <BrowserView>
+                    <h1>
+                        <span className="Flicker1A">R</span>
+                        <span>E</span>
+                        <span className="Flicker2B">T</span>
+                        <span>R</span>
+                        <span>O</span>
+                        <span>C</span>
+                        <span className="Flicker2A">A</span>
+                        <span>D</span>
+                        <span className="Flicker1B">E</span>
+                    </h1>
+                </BrowserView>
+                <MobileView>
+                    <h1>RETROCADE</h1>
+                </MobileView>
             </Link>
         </div>
         <hr />
