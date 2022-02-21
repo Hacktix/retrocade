da26dab3b9b8302e3b6f5a755401d3b905b0ae86
Hacktix <zecmad16@htlkaindorf.at>
2022-02-21 14:34:20 +0100

    Remove RETROCADE neon flicker animation on sub-pages

--- src/components/common/SiteHeader/SiteHeader.tsx
+++ src/components/common/SiteHeader/SiteHeader.tsx
@@ -1,5 +1,5 @@
 import { BrowserView, MobileView } from 'react-device-detect';
-import { Link } from 'react-router-dom';
+import { Link, Route, Routes } from 'react-router-dom';
 import './SiteHeader.css';
 
 export default function SiteHeader() {
@@ -7,22 +7,24 @@ export default function SiteHeader() {
         <>
         <div className="HeaderBar">
             <Link to="/">
-                <BrowserView>
-                    <h1>
-                        <span className="Flicker1A">R</span>
-                        <span>E</span>
-                        <span className="Flicker2B">T</span>
-                        <span>R</span>
-                        <span>O</span>
-                        <span>C</span>
-                        <span className="Flicker2A">A</span>
-                        <span>D</span>
-                        <span className="Flicker1B">E</span>
-                    </h1>
-                </BrowserView>
-                <MobileView>
-                    <h1>RETROCADE</h1>
-                </MobileView>
+                <Routes>
+                    <Route path="/" element={
+                        <h1>
+                            <span className="Flicker1A">R</span>
+                            <span>E</span>
+                            <span className="Flicker2B">T</span>
+                            <span>R</span>
+                            <span>O</span>
+                            <span>C</span>
+                            <span className="Flicker2A">A</span>
+                            <span>D</span>
+                            <span className="Flicker1B">E</span>
+                        </h1>
+                    } />
+                    <Route path="*" element={
+                        <h1>RETROCADE</h1>
+                    } />
+                </Routes>
             </Link>
         </div>
         <hr />
