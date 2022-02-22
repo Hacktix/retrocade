a48908c7aa6c4acf95114437965d8530835c35f1
Hacktix <zecmad16@htlkaindorf.at>
2022-02-22 09:31:54 +0100

    Prevent Space from scrolling page

--- src/index.tsx
+++ src/index.tsx
@@ -4,6 +4,12 @@ import './index.css';
 import App from './App';
 import { HashRouter } from 'react-router-dom';
 
+window.addEventListener('keydown', function (e) {
+    if (e.key === " " && e.target === document.body) {
+        e.preventDefault();
+    }
+});
+
 ReactDOM.render(
     <React.StrictMode>
         <HashRouter>
